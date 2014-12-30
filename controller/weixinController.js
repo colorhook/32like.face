dac042e69ee1dfb117c4866ddaabe312412d956c
正邪 <zhengxie.lj@taobao.com>
2014-12-30 23:09:22 +0800

    faceapi add 2 other resolve manner

--- controller/weixinController.js
+++ controller/weixinController.js
@@ -38,61 +38,57 @@ exports.image = function(message, callback){
   var url = 'http://f.hiphotos.baidu.com/baike/w%3D268/sign=81d9240f32fa828bd1239ae5c51e41cd/8694a4c27d1ed21b33138eefac6eddc450da3fe5.jpg'
   */
 
-  faceapi.detect(message.PicUrl, function(e, result){
+  faceapi.detect(message.PicUrl, function(e, face){
     if(e){
       logger.error(e);
-      callback(null, e.toString());
+      database.NoDetect.add({img:message.PicUrl, openid: message.FromUserName}, function(){});
+      callback(null, "花了较长时间都未找到人脸，请换一张更好的正面照片");
     }else{
       var info = '';
-      if(result.face && result.face.length){
-        var face = result.face[0];
-        var attr = face.attribute;
-        
-        database.Face.add({
-          faceid: face.face_id,
-          img: message.PicUrl,
-          data: JSON.stringify(face),
-          openid: message.FromUserName
-        }, function(err){
-          database.User.setOpenId(message.FromUserName, face.face_id, function(err){
-            info += '性别: ' + attr.gender.value;
-            info += '\n年龄: ' + attr.age.value + '<'+attr.age.range+'>';
-            info += '\n眼镜: ' + attr.glass.value;
-            info += '\n种族: ' + attr.race.value;
-            info += '\n微笑: ' + attr.smiling.value;
-            callback(null, info);
-          });
-        });
-        
-      }else{
-       logger.debug('没有通过faceplus找到人脸:'+message.PicUrl);
-       faceapi.detectByBetaFace(message.PicUrl, function(e, json){
-         if(e){
-           logger.warn('没有通过beta找到人脸:'+message.PicUrl + e.toString());
-           callback(null, '没有找到人脸');
-           database.NoDetect.add({img:message.PicUrl, openid: message.FromUserName}, function(){});
-         }else{
-           database.Face.add({
-            faceid: json.face_id,
-            img: message.PicUrl,
-            data: JSON.stringify(json),
-            openid: message.FromUserName,
-            betaface: 1,
-          }, function(err){
-            database.User.setOpenId(message.FromUserName, json.face_id, function(err){
-               info += '性别: ' + json.gender.value;
-               info += '\n年龄: ' + json.age.value;
-               info += '\n眼镜: ' + json.glasses.value;
-               info += '\n种族: ' + json.race.value;
-               info += '\n微笑: ' + json.smile.value;
-               callback(null, info);
-            });
-          });
-         }
-       });
-        
+      var type = 0;
+      if(result.type == 'betaface'){
+        type = 1;
+      }else if(result.type = 'skybiometry'){
+        type = 2;
       }
       
+      var attributes;
+      if(type == 0){
+        attributes = face.attribute;
+        info += '性别: ' + attributes.gender.value;
+        info += '\n年龄: ' + attributes.age.value;
+        info += '\n眼镜: ' + attributes.glasses.value;
+        info += '\n种族: ' + attributes.race.value;
+        info += '\n微笑: ' + attributes.smile.value + ' faceplus';
+      }else if(type == 1){
+        attributes = face.attributes;
+        info += '性别: ' + attributes.gender.value;
+        info += '\n年龄: ' +  attributes.age.value;
+        info += '\n眼镜: ' + attributes.glasses.value;
+        info += '\n种族: ' + attributes.race.value;
+        info += '\n微笑: ' + attributes.smile.value + ' betaface';
+      }else if(type == 2){
+        attributes = face.attributes;
+        info += '性别: ' + attributes.gender.value;
+        info += '\n年龄: ' +  attributes.age_est.value;
+        info += '\n眼镜: ' + attributes.glasses.value;
+        info += '\n种族: NULL' ;
+        info += '\n微笑: ' + attributes.happiness.value + ' skybiometry';
+      }
+      
+      callback(null, info);
+      database.Face.add({
+        faceid: face.face_id,
+        img: message.PicUrl,
+        data: JSON.stringify(face),
+        openid: message.FromUserName,
+        betaface: type
+      }, function(err){
+        err && logger.error(err);
+        database.User.setOpenId(message.FromUserName, face.face_id, function(err){
+          err && logger.error(err);
+        });
+      });
     }
-  });
+  }, 4000);
 }
